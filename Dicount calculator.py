3a95640cb3cc1cabe21b6516f5af31515ea863cd
siddharth-tewari <76590952+siddharth-tewari@users.noreply.github.com>
2020-12-25 19:53:01 +0530

    Add files via upload

---
+++ Dicount calculator.py
@@ -0,0 +1,5 @@
+price=float(input("Enter the price of the Item"))
+discount=float(input("Enter the amount of discount]"))
+finalprice=price*discount/100
+print("This is the amount of discount you will get=",finalprice)
+print("Thsi the final price you will have to pay=",price-finalprice)
---
+++ Square Calculator.py
@@ -0,0 +1,4 @@
+str_number=input("Enter the Number")
+number=float(str_number)
+number2=number*number
+print("This is the square of your number-",number2)
