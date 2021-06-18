7b5cdb20b3292cf64e9bbd4ac612038566735f28
siddharth-tewari <76590952+siddharth-tewari@users.noreply.github.com>
2021-06-18 10:11:31 +0530

    Add files via upload

---
+++ Circle.py
@@ -0,0 +1,6 @@
+r = float(input("Enter the Radius of the Circle: "))
+pi = (22/7)
+c = 2*pi*r
+a = pi*(r*r)
+print("Circumference of the Circle is: ",c)
+print("Area of the Circle is: ",a,"sq unit")
